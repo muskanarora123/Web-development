0e18970c6a018921965d6a3f09fab0d23c3d54c7
Muskan Arora <muskanarora98781@gmail.com>
2026-02-06 11:57:07 +0530

    script

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// const emailInput = document.getElementById("email");
-// const passwordInput = document.getElementById("password");
-// const form = document.getElementById("loginForm");
+ const emailInput = document.getElementById("email");
+ const passwordInput = document.getElementById("password");
+ const form = document.getElementById("loginForm");
 
-// form.addEventListener("submit", function (event) {
-//     event.preventDefault();
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-//     const email = emailInput.value;
-//     const password = passwordInput.value;
+    const email = emailInput.value;
+    const password = passwordInput.value;
 
-//     console.log("Email:", email);
-//     console.log("Password:", password);
-// });
+    console.log("Email:", email);
+    console.log("Password:", password);
+});
 
 // document.addEventListener("DOMContentLoaded", function () {
 
